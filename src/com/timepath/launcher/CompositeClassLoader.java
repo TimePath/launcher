7d0c6fa3ee3125be3a104204dc4dac4d111c2292
TimePath <andrew.hardaker1995@gmail.com>
2013-09-26 23:11:59 +1000

    Attempted native lib extraction. Method is never called

--- src/com/timepath/launcher/CompositeClassLoader.java
+++ src/com/timepath/launcher/CompositeClassLoader.java
@@ -1,19 +1,24 @@
 package com.timepath.launcher;
 
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.lang.reflect.Modifier;
-import java.net.JarURLConnection;
 import java.net.URL;
 import java.net.URLClassLoader;
+import java.nio.channels.Channels;
+import java.nio.channels.FileChannel;
+import java.nio.channels.ReadableByteChannel;
 import java.util.*;
-import java.util.jar.Attributes;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
 /**
  *
+ * Inspired by http://www.jdotsoft.com/JarClassLoader.php
+ * 
  * @author TimePath
  */
 public class CompositeClassLoader extends ClassLoader {
@@ -31,14 +36,15 @@ public class CompositeClassLoader extends ClassLoader {
     public void add(ClassLoader loader) {
         loaders.add(0, loader); // newest on top
     }
-    
-    private HashMap<URL, URLClassLoader> jars = new HashMap<URL, URLClassLoader>();
+
+    private HashMap<URL, ClassLoader> jars = new HashMap<URL, ClassLoader>();
 
     public void add(URL u) {
         if(jars.containsKey(u)) {
             return;
         }
-        URLClassLoader cl = URLClassLoader.newInstance(new URL[] {u}, this);
+        URLClassLoader ucl = URLClassLoader.newInstance(new URL[] {u}, this);
+        ClassLoader cl = ucl;
         jars.put(u, cl);
         add(cl);
     }
@@ -49,7 +55,7 @@ public class CompositeClassLoader extends ClassLoader {
         }
     }
 
-    public void invokeClass(String name, String[] args) throws ClassNotFoundException,
+    public void invokeMain(String name, String[] args) throws ClassNotFoundException,
                                                                NoSuchMethodException,
                                                                InvocationTargetException {
         Class c = loadClass(name);
@@ -70,13 +76,14 @@ public class CompositeClassLoader extends ClassLoader {
                                                          Arrays.toString(urls)});
         add(urls);
         try {
-            invokeClass(name, args);
+            invokeMain(name, args);
         } catch(Throwable t) {
             LOG.log(Level.SEVERE, null, t);
         }
     }
 
     private <A, B> B reflect(Map<A, B> cache, String method, A key) {
+        LOG.log(Level.INFO, "{0}: {1}", new Object[] {method, key});
         B ret = cache.get(key);
         if(cache.containsKey(key)) {
             return ret;
@@ -111,7 +118,7 @@ public class CompositeClassLoader extends ClassLoader {
 
     //<editor-fold defaultstate="collapsed" desc="Overriding methods and caches">
     private final HashMap<String, Class<?>> classes = new HashMap<String, Class<?>>();
-    
+
     @Override
     protected Class<?> findClass(String name) throws ClassNotFoundException {
         Class<?> res = reflect(classes, "findClass", name);
@@ -120,9 +127,9 @@ public class CompositeClassLoader extends ClassLoader {
         }
         return super.findClass(name);
     }
-    
+
     private final HashMap<String, URL> resources = new HashMap<String, URL>();
-    
+
     @Override
     protected URL findResource(String name) {
         URL res = reflect(resources, "findResource", name);
@@ -131,9 +138,9 @@ public class CompositeClassLoader extends ClassLoader {
         }
         return super.findResource(name);
     }
-    
+
     private final HashMap<String, Enumeration<URL>> enumerations = new HashMap<String, Enumeration<URL>>();
-    
+
     /**
      * TODO: calling class's jar only
      * <p/>
@@ -151,17 +158,33 @@ public class CompositeClassLoader extends ClassLoader {
         }
         return super.findResources(name);
     }
-    
+
     private final HashMap<String, String> libraries = new HashMap<String, String>();
-    
+
     @Override
     protected String findLibrary(String libname) {
         String res = reflect(libraries, "findLibrary", libname);
         if(res != null) {
             return res;
         }
+        String s = System.mapLibraryName(libname);
+        URL u = findResource(s);
+        try {
+            File f = File.createTempFile(libname, null);
+            f.deleteOnExit();
+            ReadableByteChannel rbc = Channels.newChannel(u.openStream());
+            FileOutputStream outputStream = new FileOutputStream(f);
+            FileChannel outputChannel = outputStream.getChannel();
+            long total = 0, read;
+            while((read = outputChannel.transferFrom(rbc, total, 8192)) > 0) {
+                total += read;
+            }
+            return f.getAbsolutePath();
+        } catch(IOException ex) {
+            Logger.getLogger(CompositeClassLoader.class.getName()).log(Level.SEVERE, null, ex);
+        }
         return super.findLibrary(libname);
     }
-    
+
     //</editor-fold>
 }
