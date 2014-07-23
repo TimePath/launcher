2bd400a03d0567fa36d81ab51b221bb3396d44d9
TimePath <andrew.hardaker1995@gmail.com>
2014-07-23 12:02:46 +1000

    Update test

--- src/test/java/com/timepath/maven/MavenResolverTest.java
+++ src/test/java/com/timepath/maven/MavenResolverTest.java
@@ -9,7 +9,6 @@ public class MavenResolverTest {
     @Test
     public void testResolve() throws Exception {
         String resolved = MavenResolver.resolve("com.timepath", "launcher", "1.0-SNAPSHOT", null);
-        assertTrue(resolved.startsWith(
-                "https://dl.dropboxusercontent.com/u/42745598/maven2/com/timepath/launcher/1.0-SNAPSHOT/launcher-1.0-"));
+        assertTrue(resolved.contains("/com/timepath/launcher/1.0-SNAPSHOT/launcher-1.0-"));
     }
 }
